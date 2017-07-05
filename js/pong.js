d7229d711d4b39ef5f05d20bce18b4b38457d606
Rob Prouse <rob@prouse.org>
2017-07-05 13:53:09 -0400

    A simple bounding ball

--- js/pong.js
+++ js/pong.js
@@ -1,1 +1,67 @@
-console.log("Hello World");
+var canvas;
+var canvasContext;
+
+var ballX;
+var ballY;
+var ballSpeed;
+
+var leftPaddleY;
+var rightPaddleY;
+
+window.onload = function() {
+  canvas = document.getElementById('game');
+  canvasContext = canvas.getContext('2d');
+
+  init();
+  var framesPerSecond = 30;
+  setInterval(gameLoop, 1000/framesPerSecond);
+}
+
+function init() {
+  ballX = 30;
+  ballY = canvas.height / 2;
+  ballSpeed = 10;
+
+  leftPaddleY = canvas.height / 2;
+  rightPaddleY = leftPaddleY;
+}
+
+function gameLoop() {
+  draw();
+  move();
+}
+
+function move() {
+  ballX += ballSpeed;
+  if(ballX >= canvas.width - 20)
+    ballSpeed = -ballSpeed;
+  else if(ballX <= 20)
+    ballSpeed = -ballSpeed;
+}
+
+function draw() {
+  drawBackground();
+  drawBall();
+  drawLeftPaddle();
+  drawRightPaddle();
+}
+
+function drawBackground() {
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawBall() {
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(ballX - 5, ballY - 5, 10, 10)
+}
+
+function drawLeftPaddle() {
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(10, leftPaddleY - 25, 10, 50)
+}
+
+function drawRightPaddle() {
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(canvas.width - 20, rightPaddleY - 25, 10, 50)
+}
